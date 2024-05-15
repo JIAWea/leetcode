71cd4da89528dacf9a8a0b858b8240b2a98be4f3
JIAWea <1552937000@qq.com>
2024-05-15 15:27:14 +0800

    arr: moveZeroes

---
+++ leetcode75/array/283_moveZeroes_test.go
@@ -0,0 +1,40 @@
+package array
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_moveZeroes(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		{
+			name: "test1",
+			args: args{
+				[]int{0, 1, 0, 3, 12},
+			},
+			want: []int{1, 3, 12, 0, 0},
+		},
+		{
+			name: "test2",
+			args: args{
+				[]int{1, 3, 0, 1, 12},
+			},
+			want: []int{1, 3, 1, 12, 0},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			moveZeroes(tt.args.nums)
+			if !reflect.DeepEqual(tt.args.nums, tt.want) {
+				t.Errorf("productExceptSelf() = %v, want %v", tt.args.nums, tt.want)
+			}
+		})
+	}
+}
